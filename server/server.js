54b2149d1017df0fab66e04c6bd8ed3ea01b956a
AHuss13 <Adamhuss13@gmail.com>
2024-07-30 12:59:56 -0500

    fixed connection to Apollo server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
-// const { expressMiddleware } = require('@apollo/server/express4');
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
 const path = require("path");
-// const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require("./utils/auth");
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
@@ -14,31 +14,34 @@ const server = new ApolloServer({
   resolvers,
 });
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// app.use('/graphql', expressMiddleware(server, {
-//   context: authMiddleware
-// }));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
 
 const startApolloServer = async () => {
   await server.start();
-  server.applyMiddleware({ app });
+  
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+  
+  app.use(
+    "/graphql",
+    expressMiddleware(server, {
+      context: authMiddleware,
+    })
+  );
+  
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
+  }
 
   db.once("open", () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
     });
   });
 };
 
-startApolloServer(typeDefs, resolvers);
+startApolloServer();
