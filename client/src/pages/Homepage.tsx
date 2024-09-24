9a86ef5107127b4ffa25e1c16f9dbfb14e4e0789
AHuss13 <Adamhuss13@gmail.com>
2024-09-24 15:56:42 -0500

    Removed unused forms from Homepage.tsx

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,30 +1,15 @@
-import { Heading, Stack, Input } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
     <>
-      <Heading size="lg" my="5px">
-        Login
-      </Heading>
-      <Stack spacing={3}>
-        <Input placeholder="Username" size="sm" />
-        <Input placeholder="Password" size="sm" />
-      </Stack>
       <Link to={"/login"}>
         <Button colorScheme="purple" my="5px">
           Login
         </Button>
       </Link>
-      <Heading size="lg" my="5px">
-        Register
-      </Heading>
-      <Stack spacing={3}>
-        <Input placeholder="Username" size="sm" />
-        <Input placeholder="Email" size="sm" />
-        <Input placeholder="Password" size="sm" />
-      </Stack>
+      <br />
       <Link to={"/signup"}>
         <Button colorScheme="purple" my="5px">
           Register
