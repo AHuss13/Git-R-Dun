1d778197a6c29db9cb0c61583e1f18a834fcf339
AHuss13 <Adamhuss13@gmail.com>
2024-09-26 15:42:44 -0500

    updated UserSchema

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -9,33 +9,40 @@ interface IUser extends Document {
   isCorrectPassword: (password: string) => Promise<boolean>;
 }
 
-const userSchema = new Schema<IUser>({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, 'Must match an email address!'],
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-  },
-  projects: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Project",
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-  ],
-});
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must match an email address!"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+    },
+    projects: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    toJSON: { getters: true },
+    toObject: { getters: true },
+  }
+);
 
-userSchema.pre("save", async function (this: IUser, next) {
+userSchema.pre<IUser>("save", async function (next) {
   if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
