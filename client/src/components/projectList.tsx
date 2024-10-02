63b31e9875f7b5ca95aa2fb5ece992458181b327
AHuss13 <Adamhuss13@gmail.com>
2024-10-02 15:40:34 -0500

    Refactor ProjectList component to display project cards with name and description

--- client/src/components/projectList.tsx
+++ client/src/components/projectList.tsx
@@ -1,57 +1,33 @@
-import react, { useContext, useState } from "react";
-import { VStack, Flex, Box } from "@chakra-ui/react";
-import { ProjectContext } from "../context/ProjectContext";
+interface Project {
+  _id: string;
+  name: string;
+  description: string;
+}
 
-function ProjectList() {
-  const { tasks, getProjectsFromDatabase, addProjectsToDatabase } =
-    useContext(ProjectContext);
+interface ProjectListProps {
+  projects: Project[];
+}
+
+const ProjectList: React.FC<ProjectListProps> = ({ projects }) => {
+  if (!projects.length) {
+    return <h3>No Projects Yet</h3>;
+  }
 
   return (
-    <VStack
-      spacing={8}
-      w="100%"
-      h="100vh"
-      bgColor="gray.100"
-      px="6"
-      py="6"
-      className="project-list"
-    >
-      {tasks.map((task) => (
-        <Flex
-          w="100%"
-          bgColor="gray.50"
-          h="80px"
-          borderRadius="10px"
-          shadow="md"
-          align="center"
-          px="5"
-          justify="space-between"
-        >
-          <Box>{project.name}</Box>
-          <Box>{project.owner}</Box>
-          <Box
-            fontSize={11}
-            color={"gray.400"}
-            width={"8%"}
-            borderRadius={"7px"}
-            textAlign="center"
-            fontWeight="bold"
-            bgColor={
-              project.status == "Done"
-                ? "green.700"
-                : project.status == "In Progress"
-                ? "blue.800"
-                : project.status == "Not Started"
-                ? "red.600"
-                : ""
-            }
-          >
-            {project.status}
-          </Box>
-        </Flex>
-      ))}
-    </VStack>
+    <div>
+      {projects &&
+        projects.map((project) => (
+          <div key={project._id} className="card mb-3">
+            <h4 className="card-header bg-primary text-light p-2 m-0">
+              {project.name}
+            </h4>
+            <h4 className="card-header bg-primary text-light p-2 m-0">
+              <p>{project.description}</p>
+            </h4>
+          </div>
+        ))}
+    </div>
   );
-}
+};
 
 export default ProjectList;
