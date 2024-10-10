6378890039024f230bc445de0e81272359489317
AHuss13 <Adamhuss13@gmail.com>
2024-10-10 14:10:31 -0500

    Refactor projectList component to use Chakra UI and add links to project details

--- client/src/components/projectList.tsx
+++ client/src/components/projectList.tsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import { Box, Heading, Text, VStack } from "@chakra-ui/react";
+
 interface Project {
   _id: string;
   name: string;
@@ -10,23 +13,36 @@ interface ProjectListProps {
 
 const ProjectList: React.FC<ProjectListProps> = ({ projects }) => {
   if (!projects.length) {
-    return <h3>No Projects Yet</h3>;
+    return <Heading as="h3">No Projects Yet</Heading>;
   }
 
   return (
-    <div>
-      {projects &&
-        projects.map((project) => (
-          <div key={project._id} className="card mb-3">
-            <h4 className="card-header bg-primary text-light p-2 m-0">
-              {project.name}
-            </h4>
-            <h4 className="card-header bg-primary text-light p-2 m-0">
-              <p>{project.description}</p>
-            </h4>
-          </div>
-        ))}
-    </div>
+    <VStack spacing={4} align="stretch">
+      {projects.map((project) => (
+        <Box
+          key={project._id}
+          borderWidth="1px"
+          borderRadius="lg"
+          overflow="hidden"
+        >
+          <Box bg="primary.500" _dark={{ bg: "primary.700" }} p={2}>
+            <Heading
+              as="h4"
+              size="md"
+              color="gray.800"
+              _dark={{ color: "gray.100" }}
+            >
+              <Link to={`/project/${project._id}`}>{project.name}</Link>
+            </Heading>
+          </Box>
+          <Box p={2} bg="gray.50" _dark={{ bg: "gray.700" }}>
+            <Text color="gray.800" _dark={{ color: "gray.200" }}>
+              {project.description}
+            </Text>
+          </Box>
+        </Box>
+      ))}
+    </VStack>
   );
 };
 
