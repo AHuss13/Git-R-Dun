69a8d7999dfbdd303a9590f5a83be235b8d2b9c7
AHuss13 <Adamhuss13@gmail.com>
2024-10-10 14:10:46 -0500

    Refactor Userpage component to use Chakra UI and add project creation form

--- client/src/pages/Userpage.tsx
+++ client/src/pages/Userpage.tsx
@@ -1,143 +1,176 @@
-import { useQuery } from "@apollo/client";
+import { useState, FormEvent, useEffect } from "react";
+import { useQuery, useMutation } from "@apollo/client";
 import {
   Heading,
-  Stack,
   Card,
-  CardHeader,
   CardBody,
-  Container,
   Box,
   Text,
   Input,
+  VStack,
+  Container,
+  Spinner,
+  FormControl,
+  FormLabel,
   Select,
+  Button,
 } from "@chakra-ui/react";
-import { Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
-import { QUERY_PROJECTS } from "../utils/queries";
-// import { Header } from "../components/Header";
+import { QUERY_PROJECTS, QUERY_USERS } from "../utils/queries";
+import { ADD_PROJECT } from "../utils/mutations";
 import ProjectList from "../components/projectList";
 
+interface User {
+  _id: string;
+  username: string;
+  email: string;
+}
+
 const Userpage = () => {
-  const { loading, data } = useQuery(QUERY_PROJECTS);
-  const projects = data?.projects || [];
+  const { loading: projectsLoading, data: projectsData, error: projectsError } = useQuery(QUERY_PROJECTS);
+  const { loading: usersLoading, data: usersData, error: usersError } = useQuery(QUERY_USERS);
+  const projects = projectsData?.projects || [];
+  const users = usersData?.users || [];
 
-  // Display projects if logged in
-  if (Auth.loggedIn()) {
+  const [formState, setFormState] = useState({
+    name: "",
+    description: "",
+    owner: "",
+  });
+
+  const [addProject, { error: addProjectError }] = useMutation(ADD_PROJECT, {
+    refetchQueries: [{ query: QUERY_PROJECTS }],
+  });
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const { data } = await addProject({
+        variables: { 
+          input: {
+            name: formState.name,
+            description: formState.description,
+            owner: formState.owner,
+          },
+        },
+      });
+      console.log("Project added:", data);
+      // Clear form fields after successful submission
+      setFormState({
+        name: "",
+        description: "",
+        owner: "",
+      });
+    } catch (e) {
+      console.error("Error adding project:", e);
+    }
+  };
+
+  if (projectsLoading || usersLoading) {
+    return <Spinner />;
+  }
+
+  if (projectsError) {
+    console.error("Error fetching projects:", projectsError);
+  }
+
+  if (usersError) {
+    console.error("Error fetching users:", usersError);
+  }
+
+  if (addProjectError) {
+    console.error("Error adding project:", addProjectError);
+  }
+
+  if (!Auth.loggedIn()) {
     return (
-      <>
-        <ProjectList projects={projects} />
-
-        <Text fontSize="xl">--------Below this is just Filler!!!--------</Text>
-        <Card>
-          <CardHeader>
-            <Heading size="md">User Page</Heading>
-          </CardHeader>
-
-          <CardBody>
-            <Stack spacing="4">
-              <Box>
-                <Heading size="xs" textTransform="uppercase">
-                  Create New Project
-                </Heading>
-                <Stack spacing={3}>
-                  <Input placeholder="Project Name" size="sm" />
-                  <Input placeholder="Project Description" size="sm" />
-                  <Select placeholder="Select Members">
-                    <option value="option1">User 1</option>
-                    <option value="option2">User 2</option>
-                    <option value="option3">User 3</option>
-                  </Select>
-                </Stack>
-                <Link to={"/projectpage"}>
-                  <Button colorScheme="purple" my="5px">
-                    Create Project{" "}
-                  </Button>
-                </Link>
-              </Box>
-              <Box>
-                <Container></Container>
-              </Box>
-              <Box>
-                <Heading size="xs" textTransform="uppercase">
-                  Current Projects
-                </Heading>
-                <Text pt="2" fontSize="sm">
-                  <ul>
-                    <li>
-                      <Stack
-                        spacing={4}
-                        direction="row"
-                        align="center"
-                        my="2px"
-                      >
-                        <Link to={"/projectpage"}>
-                          <Button colorScheme="teal" size="sm">
-                            Project 1
-                          </Button>
-                        </Link>
-                      </Stack>
-                    </li>
-                    <li>
-                      <Stack
-                        spacing={4}
-                        direction="row"
-                        align="center"
-                        my="2px"
-                      >
-                        <Link to={"/projectpage"}>
-                          <Button colorScheme="teal" size="sm">
-                            Project 2
-                          </Button>
-                        </Link>
-                      </Stack>
-                    </li>
-                    <li>
-                      <Stack
-                        spacing={4}
-                        direction="row"
-                        align="center"
-                        my="2px"
-                      >
-                        <Link to={"/projectpage"}>
-                          <Button colorScheme="teal" size="sm">
-                            Project 3
-                          </Button>
-                        </Link>
-                      </Stack>
-                    </li>
-                  </ul>
-                </Text>
-              </Box>
-              <Stack spacing={4} direction="row" align="center" my="2px">
-                <Link to={"/"}>
-                  <Button colorScheme="blue" size="sm">
-                    Home Page
-                  </Button>
-                </Link>
-              </Stack>
-            </Stack>
-          </CardBody>
-        </Card>
-      </>
+      <Container>
+        <Text fontSize="xl">
+          You need to be logged in to see this. Use the navigation links above
+          to sign up or log in!
+        </Text>
+      </Container>
     );
   }
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  return (
+    <Container maxW="container.lg">
+      <VStack spacing={8} align="stretch">
+        <ProjectList projects={projects} />
 
-  // if (!user?.username) {
-  //   return (
-  //     <>
-  //       <Header />
-  //       <h4>
-  //         You need to be logged in to see this. Use the navigation links above
-  //         to sign up or log in!
-  //       </h4>
-  //     </>
-  //   );
-  // }
-}
+        <Card>
+          <CardBody>
+            <VStack spacing={4} align="stretch">
+              <Heading size="md">Create a New Project</Heading>
+              <form onSubmit={handleFormSubmit}>
+                <VStack spacing={3}>
+                  <FormControl isRequired>
+                    <FormLabel>Project Name</FormLabel>
+                    <Input
+                      placeholder="Enter project name"
+                      name="name"
+                      value={formState.name}
+                      onChange={handleChange}
+                    />
+                  </FormControl>
+                  <FormControl isRequired>
+                    <FormLabel>Project Description</FormLabel>
+                    <Input
+                      placeholder="Enter project description"
+                      name="description"
+                      value={formState.description}
+                      onChange={handleChange}
+                    />
+                  </FormControl>
+                  <FormControl isRequired>
+                    <FormLabel>Project Owner</FormLabel>
+                    <Select
+                      placeholder="Select project owner"
+                      name="owner"
+                      value={formState.owner}
+                      onChange={handleChange}
+                    >
+                      {users.length > 0 ? (
+                        users.map((user: User) => (
+                          <option key={user._id} value={user._id}>
+                            {user.username}
+                          </option>
+                        ))
+                      ) : (
+                        <option disabled>No users available</option>
+                      )}
+                    </Select>
+                  </FormControl>
+                  <Button type="submit" colorScheme="purple" width="full">
+                    Create Project
+                  </Button>
+                </VStack>
+              </form>
+            </VStack>
+          </CardBody>
+        </Card>
+        {addProjectError && (
+          <Box>
+            <Text color="red.500">Error creating project: {addProjectError.message}</Text>
+          </Box>
+        )}
+        <Button as={Link} to="/" colorScheme="blue">
+          Home Page
+        </Button>
+      </VStack>
+    </Container>
+  );
+};
 
 export default Userpage;
