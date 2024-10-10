6dabcf19b0ad059eed713c33e971643abd88bfaf
AHuss13 <Adamhuss13@gmail.com>
2024-10-10 14:12:27 -0500

    Refactor User, Project, and Task models to include required fields and update resolvers and queries accordingly

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -2,9 +2,9 @@ import { gql } from "apollo-server-express";
 
 const typeDefs = gql`
   type User {
-    _id: ID
-    username: String
-    email: String
+    _id: ID!
+    username: String!
+    email: String!
     password: String
     projects: [Project]!
   }
@@ -24,31 +24,30 @@ const typeDefs = gql`
     _id: ID!
     name: String!
     description: String
-    owner: User!
-    members: [User]
+    owner: User
+    members: [User]!
     createdAt: String!
-    tasks: [Task]
+    tasks: [Task]!
   }
 
   input ProjectInput {
     name: String!
     description: String!
-    owner: ID!
-    members: [ID]
+    owner: ID
   }
 
   type Task {
     _id: ID!
     name: String!
     status: String!
-    owner: String
-    createdAt: String
+    owner: User
+    createdAt: String!
   }
 
   type Query {
-    users: [User]
-    user(username: String!): User
-    # user(id: ID!): User
+    users: [User]!
+    user(userId: ID!): User
+    me: User
     projects: [Project]!
     project(projectId: ID!): Project
     tasks: [Task]!
@@ -56,16 +55,18 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    addUser(input: UserInput!): Auth
-    # removeUser(userId: ID!): User
     login(email: String!, password: String!): Auth
-    addProject(
-      name: String!
-      description: String!
-      owner: ID!
+    addUser(input: UserInput!): Auth
+    addProject(input: ProjectInput!): Project
+    addTask(name: String!, projectId: ID!, status: String): Task
+    updateProject(
+      projectId: ID!
+      name: String
+      description: String
       members: [ID]
+      owner: ID
     ): Project
-    addTask(name: String!, projectId: ID!, owner: ID, status: String): Task
+    removeUser(userId: ID!): User
     removeProject(projectId: ID!): Project
     removeTask(taskId: ID!): Task
   }
