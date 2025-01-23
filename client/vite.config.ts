15e291734d2c83dfae07a699689e87ac297c4560
AHuss13 <Adamhuss13@gmail.com>
2025-01-22 20:15:47 -0600

    Updated Homepage and Login pages with user instructions; modified CSS for login info display; adjusted Vite config for consistency and updated TypeScript version.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,9 +7,9 @@ export default defineConfig({
     port: 3000,
     open: true,
     proxy: {
-       // We have to set up our Vite client-side development server to proxy API requests to our server-side Node server at port 3001 in order to hit our GraphQL application.
-      '/graphql': {
-        target: 'http://localhost:3001',
+      // We have to set up our Vite client-side development server to proxy API requests to our server-side Node server at port 3001 in order to hit our GraphQL application.
+      "/graphql": {
+        target: "http://localhost:3001",
         changeOrigin: true,
         secure: false,
       },
